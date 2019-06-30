fedabb67c492fdbeec3086518ca0e9e18decfe16
Jeremy Zevin <jz@jz.fios-router.home>
2019-06-29 22:09:17 -0400

    app state plus Panels

--- app.js
+++ app.js
@@ -1,27 +1,66 @@
 import { View } from './lib/View.mjs';
 import { CanvasDocument } from "./lib/CanvasDocument.mjs";
+import { LayersPanel } from './lib/LayersPanel.mjs';
+
+class State {
+    _currentDocument;
+    constructor(document) {
+        this.currentDocument = document;
+    }
+    set currentDocument(document) {
+        this._currentDocument = document;
+    }
+    get currentDocument() {
+        return this._currentDocument;
+    }
+}
 
 class App {
     
+    state;
+    view;
     observers = [];
     documents = [];
+    panels = [];
 
-    constructor(params) {
+    constructor() {
         this.view = new View(this, document.querySelector('#app'));
-        this.documents.push(
-            new CanvasDocument(this, this.view.dom.documentsEl, 500, 400)
+        const doc = new CanvasDocument(
+            this,
+            this.view.dom.documentsEl,
+            500, 400
         );
+        const layersPanel = new LayersPanel(
+            this,
+            'Layers', 
+            this.view.dom.panelsEl
+        );
+        this.state = new State(doc);
+        this.documents.push(doc);
+        this.panels.push(layersPanel);
         document.addEventListener('mousemove', e => this.onEvent(e));
         document.addEventListener('mousedown', e => this.onEvent(e));
         document.addEventListener('mouseup', e => this.onEvent(e));
+        window.requestAnimationFrame(t => this.onEvent(t));
     }
 
     onEvent(e) {
-        this.observers.forEach(obs => {
-            if(obs.type === e.type) {
-                obs.cb(e);
-            }
-        });
+        if(e instanceof MouseEvent) {
+            this.observers.forEach(obs => {
+                if(obs.type === e.type) {
+                    obs.cb(e);
+                }
+            });
+        } else {
+            // NOTE: This could be optimized with dif sub array
+            // but think fine for now since list is small
+            window.requestAnimationFrame(t => this.onEvent(t));
+            this.observers.forEach(obs => {
+                if(obs.type === 'raf') {
+                    obs.cb(e);
+                }
+            });
+        }
     }
 
     subscribe(eventName, cb) {
